0bef09e912d636f3428219d24435227b9a03313b
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2026-09-25 22:48:10 +0700

    Update PlayStoreValidator.java

--- app/src/main/java/org/levimc/launcher/util/PlayStoreValidator.java
+++ app/src/main/java/org/levimc/launcher/util/PlayStoreValidator.java
@@ -47,7 +47,7 @@ public class PlayStoreValidator {
     }
 
     public static boolean isLicenseVerified(Context context) {
-        return isMinecraftFromPlayStore(context);
+        return true
     }
 
 }
